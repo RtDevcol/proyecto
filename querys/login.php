2b0de318c9450ba1109e42021a383b2a584fc9ec
RtDevcol <jonadev044@gmail.com>
2024-10-11 14:40:51 -0600

    modified:   querys/login

--- querys/login.php
+++ querys/login.php
@@ -4,6 +4,24 @@ include('../config/conexion.php');
 $query = "SELECT * FROM usuarios";
 $resultado = @mysqli_query($conexion, $query);
 
-echo $resultado;
+// Verifica si la consulta fue exitosa
+if ($resultado) {
+    // Itera sobre los resultados y muestra cada fila
+    while ($fila = mysqli_fetch_assoc($resultado)) {
+        // Aquí puedes mostrar los campos que desees. Por ejemplo:
+        echo "ID: " . $fila['id'] . "<br>";
+        echo "Usuario: " . $fila['usuario'] . "<br>";
+        echo "Contraseña: " . $fila['password'] . "<br><br>";
+        echo "Nivel: " . $fila['tipo'] . "<br><br>";
+    }
+    // Libera el resultado
+    mysqli_free_result($resultado);
+} else {
+    // Si hubo un error en la consulta, lo puedes mostrar aquí
+    echo "Error en la consulta: " . mysqli_error($conexion);
+}
+
+// Cierra la conexión
+mysqli_close($conexion);
 
 ?>
